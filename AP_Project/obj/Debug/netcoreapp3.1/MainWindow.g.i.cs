353cc1640fedd990961bc5ce8b701d1c5f8a6efe
hypensanez12 <S00189824@mail.itsligo.ie>
2022-05-01 16:30:19 +0100

    erew

--- AP_Project/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ AP_Project/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -85,7 +85,7 @@ namespace AP_Project {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AP_Project;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AP_Project;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
